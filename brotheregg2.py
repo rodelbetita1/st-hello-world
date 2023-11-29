22dd39b59a69f683206e73db04a2eff8fc9709f5
rodelbetita1 <152369797+rodelbetita1@users.noreply.github.com>
2023-11-29 14:47:37 +0800

    Create brotheregg2.py

---
+++ brotheregg2.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+# ---- HEADER SECTION ----
+st.subheader("Hi, I am Rodel  :wave:")
+st.title("A Student from SNSU")
+st.write("I am passionate to learn from this school")
